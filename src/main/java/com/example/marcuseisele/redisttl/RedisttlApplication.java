30c27215e0c8bc64adedfb9e61bd364f08806620
Marcus Eisele <marcus.eisele@gmail.com>
2018-12-12 20:13:10 +0100

    INIT

---
+++ src/main/java/com/example/marcuseisele/redisttl/RedisttlApplication.java
@@ -0,0 +1,14 @@
+package com.example.marcuseisele.redisttl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisttlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisttlApplication.class, args);
+	}
+
+}
+
